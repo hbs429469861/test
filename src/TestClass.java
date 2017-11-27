cb69755ec375c310f0871cd78ebbcba664fc3d9e
hbsong <429469861@qq.com>
2017-11-27 13:16:11 +0800

    测试idea提交 第二次

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,5 +3,7 @@ public class TestClass {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("hello");
+        System.out.println("sdasd");
+        System.out.println("sdasd");
     }
 }
